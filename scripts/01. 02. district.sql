2fb1b24d24590176b31a43bbf6648876ecba9f3c
madhavappaneni <appanenim@gmail.com>
2022-11-28 18:07:11 -0500

    split district table

---
+++ scripts/01. 02. district.sql
@@ -0,0 +1,85 @@
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (1,'New York City','New York'),
+	 (2,'Jacksonville','Florida'),
+	 (3,'Columbus','Ohio'),
+	 (4,'Charlotte','North Carolina'),
+	 (5,'Indianapolis','Indiana'),
+	 (6,'Seattle','Washington'),
+	 (7,'Denver','Colorado'),
+	 (8,'Washington','District of Columbia'),
+	 (9,'Boston','Massachusetts'),
+	 (10,'Detroit','Michigan');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (11,'Nashville','Tennessee'),
+	 (12,'Portland','Oregon'),
+	 (13,'Oklahoma City','Oklahoma'),
+	 (14,'Las Vegas','Nevada'),
+	 (15,'Louisville','Kentucky'),
+	 (16,'Baltimore','Maryland'),
+	 (17,'Milwaukee','Wisconsin'),
+	 (18,'Albuquerque','New Mexico'),
+	 (19,'Atlanta','Georgia'),
+	 (20,'Kansas City','Missouri');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (21,'Omaha','Nebraska'),
+	 (22,'Virginia Beach','Virginia'),
+	 (23,'Minneapolis','Minnesota'),
+	 (24,'New Orleans','Louisiana'),
+	 (25,'Wichita','Kansas'),
+	 (26,'Honolulu','Hawaii'),
+	 (27,'San Antonio','Texas'),
+	 (28,'Anchorage','Alaska'),
+	 (29,'Newark','New Jersey'),
+	 (30,'Boise','Idaho');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (31,'Des Moines','Iowa'),
+	 (32,'Birmingham','Alabama'),
+	 (33,'Salt Lake City','Utah'),
+	 (34,'Little Rock','Arkansas'),
+	 (35,'Sioux Falls','South Dakota'),
+	 (36,'Providence','Rhode Island'),
+	 (37,'Jackson','Mississippi'),
+	 (38,'Bridgeport','Connecticut'),
+	 (39,'Charleston','South Carolina'),
+	 (40,'Fargo','North Dakota');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (41,'Manchester','New Hampshire'),
+	 (42,'Billings','Montana'),
+	 (43,'Wilmington','Delaware'),
+	 (44,'Portland','Maine'),
+	 (45,'Cheyenne','Wyoming'),
+	 (46,'San Diego','California'),
+	 (47,'Charleston','West Virginia'),
+	 (48,'Worcester','Massachusetts'),
+	 (49,'Burlington','Vermont'),
+	 (50,'Buffalo','New York');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (51,'Springfield','Massachusetts'),
+	 (52,'New Haven','Connecticut'),
+	 (53,'Hartford','Connecticut'),
+	 (54,'Houston','Texas'),
+	 (55,'Lowell','Massachusetts'),
+	 (56,'Cambridge','Massachusetts'),
+	 (57,'New Bedford','Massachusetts'),
+	 (58,'Brockton','Massachusetts'),
+	 (59,'Quincy','Massachusetts'),
+	 (60,'Lynn','Massachusetts');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (61,'Fall River','Massachusetts'),
+	 (62,'Nashua','New Hampshire'),
+	 (63,'Norwalk','Connecticut'),
+	 (64,'Phoenix','Arizona'),
+	 (65,'Newton','Massachusetts'),
+	 (66,'Warwick','Rhode Island'),
+	 (67,'Danbury','Connecticut'),
+	 (68,'Cranston','Rhode Island'),
+	 (69,'Lawrence','Massachusetts'),
+	 (70,'Los Angeles','California');
+INSERT INTO public.district (district_id,city,state_name) VALUES
+	 (71,'Somerville','Massachusetts'),
+	 (72,'Philadelphia','Pennsylvania'),
+	 (73,'New Britain','Connecticut'),
+	 (74,'Chicago','Illinois'),
+	 (75,'Rochester','New York'),
+	 (76,'Yonkers','New York'),
+	 (77,'Syracuse','New York');
